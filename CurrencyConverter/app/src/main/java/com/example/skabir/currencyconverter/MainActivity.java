a595a9db5037125f4250de091891beb4164c1314
Sharif Kabir <sharifkbr@gmail.com>
2017-03-07 15:21:40 -0500

    first try

---
+++ CurrencyConverter/app/src/main/java/com/example/skabir/currencyconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.skabir.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert (View view){
+
+        EditText dollarAmountEditText = (EditText) findViewById(R.id.dollarAmountText);
+
+        Double dollarAmountDouble = Double.parseDouble(dollarAmountEditText.getText().toString());
+
+        Double poundAmount = dollarAmountDouble * 0.75;
+
+        Toast.makeText(MainActivity.this, poundAmount.toString(), Toast.LENGTH_LONG).show();
+
+        Log.i("amount", dollarAmountEditText.getText().toString());
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
